e0f6aeafa211c55ef42f55885398e5e04c0bceac
skcheung <scheung@qmo.io>
2020-02-23 00:23:19 -0500

    feat(server): add body-parser and cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 import router from "./routes";
-import { bodyFormatter, errorHandler } from "./middlewares";
+import { bodyFormatter, errorHandler, logger } from "./middlewares";
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
 app.use(bodyFormatter);
+app.use(logger);
 
 app.use("/", router); // path must route to lambda
 app.use("/.netlify/functions/server", router); // path must route to lambda
