d97857bc243067b363c6738b084c85ea70f250e4
skcheung <scheung@qmo.io>
2020-02-23 15:47:57 -0500

    feat(register.con): model

--- src/routes/auth/register.con.ts
+++ src/routes/auth/register.con.ts
@@ -18,14 +18,12 @@ const registerValidator = [
 
 const transformCreateData = async (user: User) => {
   const { username, password } = user;
-  const data = await this.model.find({ username });
+  const data = await User.find({ username });
   if (Array.isArray(data) && data.length > 0)
     throw { message: "Username already exists" };
 
   const saltRounds = Number(process.env.SALT_ROUNDS || "2");
   const extendedData = {
-    firstName: "",
-    lastName: "",
     ...user,
     password: bcryptjs.hashSync(password, saltRounds)
   };
