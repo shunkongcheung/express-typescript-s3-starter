9caf179c6005433432772ead697a5007a56c4c57
skcheung <scheung@qmo.io>
2020-02-23 15:29:06 -0500

    feat(base.ent): not as entity

--- src/entities/base.ent.ts
+++ src/entities/base.ent.ts
@@ -1,6 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, BaseEntity } from "typeorm";
+import { PrimaryGeneratedColumn, BaseEntity } from "typeorm";
 
-@Entity()
 abstract class Base extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
