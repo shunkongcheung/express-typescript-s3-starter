37e3985ced639e930e7c9a5fadf9ff888370d308
skcheung <scheung@qmo.io>
2020-02-23 01:41:28 -0500

    feat(server): dotenv and initDb

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 
+import initDb from "./init-db";
 import router from "./routes";
 import { bodyFormatter, errorHandler, logger } from "./middlewares";
 
+// configuration initializiation
+dotenv.config();
+
+// express app initializiation
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,9 +19,20 @@ app.use(cors());
 app.use(bodyFormatter);
 app.use(logger);
 
+// create routes
 app.use("/", router); // path must route to lambda
 app.use("/.netlify/functions/server", router); // path must route to lambda
 
 app.use(errorHandler); // error handling. after all route
 
+// database initializiation
+initDb({
+  host: "localhost",
+  port: 5432,
+  database: "express_starter",
+  username: "express_starter_user",
+  password: process.env.DB_PASSWORD || ""
+});
+
+// finish and export
 module.exports = app;
