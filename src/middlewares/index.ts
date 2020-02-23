cd79250dd0e73bc9d046485fb095065442ed10b7
skcheung <scheung@qmo.io>
2020-02-23 03:22:10 -0500

    feat(auth.mid): init

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,7 @@
+import auth from "./auth.mid";
 import bodyFormatter from "./body-formatter.mid";
 import errorHandler from "./error-handler.mid";
 import logger from "./logger.mid";
 import validateRequest from "./validate-request.mid";
 
-export { bodyFormatter, errorHandler, logger, validateRequest };
+export { auth, bodyFormatter, errorHandler, logger, validateRequest };
