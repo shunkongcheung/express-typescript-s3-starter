8238ef56dee7efb6fbae0d905c074c9c1e0d6879
shun kong cheung <shunkong.cheung@gmail.com>
2020-02-28 17:54:18 -0500

    feat(entities): Entities to entityGenerators

---
+++ src/entities/getFileEntity.ts
@@ -0,0 +1,23 @@
+import { Column } from "typeorm";
+
+import BaseUser from "./BaseUser";
+import getBaseEntity from "./getBaseEntity";
+
+function getFileEntity<T extends typeof BaseUser>(userModel: T) {
+  class BaseFile extends getBaseEntity(userModel) {
+    @Column()
+    name: string;
+
+    @Column()
+    fileType: "image" | "document";
+
+    @Column()
+    s3Key: string;
+
+    @Column("text")
+    url: string; // url to s3
+  }
+  return BaseFile;
+}
+
+export default getFileEntity;
