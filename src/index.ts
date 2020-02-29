ae6dd9e6275849454d53f343c699f0aa5688b4cb
shun kong cheung <shunkong.cheung@gmail.com>
2020-02-28 21:36:54 -0500

    feat(index): add an index file

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { getBaseEntity, getFileEntity, BaseUser } from "./entities";
+import getController from "./getController";
+import getRoutes from "./getRoutes";
+import getExpressApp from "./getExpressApp";
+
+export {
+  getBaseEntity,
+  getFileEntity,
+  BaseUser,
+  getController,
+  getRoutes,
+  getExpressApp
+};
