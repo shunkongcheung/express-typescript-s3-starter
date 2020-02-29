415d3bab96d67435181fcab0855c61e7012b3e66
shun kong cheung <shunkong.cheung@gmail.com>
2020-02-29 00:29:49 -0500

    feat(hello): update message

--- example/routes/hello.ts
+++ example/routes/hello.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from "express";
 const router = express.Router();
 
 router.all("/", (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: "this is index page" });
+  res.json({ message: "this is an example of index page" });
   next();
 });
 
