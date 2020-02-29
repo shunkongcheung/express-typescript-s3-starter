56cde52884a8f5de9f4eee3ed46ce0294db6a640
shun kong cheung <shunkong.cheung@gmail.com>
2020-02-29 01:08:12 -0500

    feat(server): provide fileModel

--- example/server.ts
+++ example/server.ts
@@ -1,10 +1,11 @@
 import { getExpressApp } from "../src";
 
 import router from "./routes";
-import { User } from "./entities";
+import { File, User } from "./entities";
 
 // create routes
 const { app, serverlessHandler } = getExpressApp({
+  fileModel: File,
   router,
   userModel: User
 });
