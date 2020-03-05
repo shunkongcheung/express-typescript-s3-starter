e257b318cfea8ec74805c2e96058dae0a41ba6e2
shun cheung <shunkong.cheung@gmail.com>
2020-03-04 21:43:13 -0500

    Fix/get option catch (#16)
    * feat(getOptionsController): catch one by one
    
    * feat(getOptionsController): catch option one by one
    
    * feat(routes/todo): example of giving invalid middleware

--- example/routes/todos.ts
+++ example/routes/todos.ts
@@ -3,11 +3,15 @@ import { body } from "express-validator";
 import getController from "../getController";
 import { Todo } from "../entities";
 
-const validations = [body("name").isString(), body("content").isString()];
+const validations = [
+  body("name").isString(),
+  body("content").isString(),
+  (req: any, res: any, next: any) => next()
+];
 
 const controller = getController({
   model: Todo,
-  validations: { create: validations, update: validations }
+  validations: { create: validations, update: validations } as any
 });
 
 export default controller;
